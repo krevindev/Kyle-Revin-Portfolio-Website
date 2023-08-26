ade6ddf180ae412e03fce8cec00dd6f31e450d65
Kyle Revin <kylitadavinci@gmail.com>
2023-08-26 19:41:47 +0800

    Changed the about contact button text cta to Connect Now

--- src/components/about_contact/AboutContactContainer.js
+++ src/components/about_contact/AboutContactContainer.js
@@ -5,7 +5,7 @@ export default function AboutContactContainer({ setIsContactVisible }) {
         <div id='about-contact-container'>
             <span>BRING YOUR VISION TO LIFE</span>
             <h5>Let's Collaborate!</h5>
-            <button className='my-default-btn' onClick={() => setIsContactVisible(true)}>Let's Talk</button>
+            <button className='my-default-btn' onClick={() => setIsContactVisible(true)}>Connect Now</button>
         </div>
     )
 }
