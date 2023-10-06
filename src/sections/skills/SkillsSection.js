30f4680727c55ff3e02bc5a1082b84e8b85ca3e8
Kyle Revin <alimpuangonkylerevin@gmail.com>
2023-10-06 13:18:20 +0800

    renamed the imgSrc properties

--- src/sections/skills/SkillsSection.js
+++ src/sections/skills/SkillsSection.js
@@ -13,94 +13,94 @@ export default function Skills() {
         techStack: [
             {
                 skillName: 'HTML5',
-                imgSrc: 'html5-logo.svg',
+                imgFilename: 'html5-logo.svg',
                 level: 80,
                 description: "HTML5 is the modern standard for structuring web content."
             },
             {
                 skillName: 'CSS3',
-                imgSrc: 'css-logo.svg',
+                imgFilename: 'css-logo.svg',
                 level: 80,
                 description: "CSS3 is the latest version of Cascading Style Sheets, bringing advanced styling and layout features to enhance web design."
             },
             {
                 skillName: 'JavaScript',
-                imgSrc: 'javascript-logo.svg',
+                imgFilename: 'javascript-logo.svg',
                 level: 75,
                 description: "JavaScript is a versatile scripting language for web development, enabling interactive and dynamic website functionality."
             },
             {
                 skillName: 'TypeScript',
-                imgSrc: 'typescript-logo.svg',
+                imgFilename: 'typescript-logo.svg',
                 level: 40,
                 description: "A powerful superset of JavaScript that brings static typing, enhanced tooling, and improved scalability to your web development projects."
             },
             {
                 skillName: 'ReactJS',
-                imgSrc: 'react-logo.svg',
+                imgFilename: 'react-logo.svg',
                 level: 60,
                 description: "React is a free and open-source front-end JavaScript library for building user interfaces based on components."
             },
             {
                 skillName: 'NodeJS',
-                imgSrc: 'nodejs-logo.svg',
+                imgFilename: 'nodejs-logo.svg',
                 level: 65,
                 description: "Runtime environment that lets you execute JavaScript on the server side, enabling dynamic web content."
             },
             {
                 skillName: 'MongoDB',
-                imgSrc: 'mongoDB-logo.svg',
+                imgFilename: 'mongoDB-logo.svg',
                 level: 60,
                 description: "NoSQL database solution used to store, retrieve, and manage data in flexible, JSON-like documents."
             },
             {
                 skillName: 'Firebase',
-                imgSrc: 'firebase-logo.svg',
+                imgFilename: 'firebase-logo.svg',
                 level: 35,
                 description: "Platform offering tools and services to develop high-quality web and mobile apps, including authentication, real-time databases, and hosting."
             },
             {
                 skillName: 'ExpressJS',
-                imgSrc: 'expressJS-logo.svg',
+                imgFilename: 'expressJS-logo.svg',
                 level: 60,
                 description: " Minimalist web application framework for Node.js, simplifying the creation of server-side applications."
             },
             {
                 skillName: 'Python',
-                imgSrc: 'python-logo.svg',
+                imgFilename: 'python-logo.svg',
                 level: 76,
                 description: " General-purpose programming language known for its readability and versatility, suitable for various applications, from web development to data analysis."
             },
             {
                 skillName: 'JQuery',
-                imgSrc: 'jquery-logo.svg',
+                imgFilename: 'jquery-logo.svg',
                 level: 30,
                 description: "JavaScript library simplifying web development tasks and interactions."
             }
             ,
             {
                 skillName: 'NextJS',
-                imgSrc: 'nextJS-logo.svg',
+                imgFilename: 'nextJS-logo.svg',
                 level: 20,
                 description: "React framework for building server-rendered and optimized web applications."
             }
             ,
             {
                 skillName: 'PHP',
-                imgSrc: 'php-logo.svg',
+                imgFilename: 'php-logo.svg',
                 level: 15,
                 description: "Server-side scripting language for dynamic web content and application development."
             }
             ,
             {
                 skillName: 'SQL',
-                imgSrc: 'sqlLogo.svg',
+                imgFilename: 'sqlLogo.svg',
                 level: 40,
                 description: "Structured Query Language used for managing and querying relational databases."
             },
             {
                 skillName: 'Tailwind CSS',
-                imgSrc: 'tailwind-logo.svg',
+                imgFilename: 'tailwind-logo.svg',
                 level: 20,
                 description: "Tailwind CSS is a utility-first CSS framework utilized for efficiently styling web interfaces."
             }
@@ -109,60 +109,60 @@ export default function Skills() {
         tools: [
             {
                 skillName: 'Adobe Photoshop',
-                imgSrc: 'photoshop-logo.svg',
+                imgFilename: 'photoshop-logo.svg',
                 level: 80,
                 description: " Industry-standard software for image editing and graphic design."
             },
             {
                 skillName: 'Figma',
-                imgSrc: 'figma-logo.svg',
+                imgFilename: 'figma-logo.svg',
                 level: 70,
                 description: "Collaborative design tool used for creating user interfaces and prototypes."
             },
             {
                 skillName: 'Postman',
-                imgSrc: 'postman-logo.svg',
+                imgFilename: 'postman-logo.svg',
                 level: 70,
                 description: " API development and testing platform for simplifying API interactions."
             }
             ,
             {
                 skillName: 'Github',
-                imgSrc: 'github-logo.svg',
+                imgFilename: 'github-logo.svg',
                 level: 55,
                 description: "Web-based platform for version control and collaborative software development."
             }
             ,
             {
                 skillName: 'Git',
-                imgSrc: 'git-logo.svg',
+                imgFilename: 'git-logo.svg',
                 level: 40,
                 description: " Distributed version control system widely used for tracking changes in code, enabling collaboration, and managing software development workflows."
             }, {
                 skillName: 'Glitch',
-                imgSrc: 'glitch-logo.svg',
+                imgFilename: 'glitch-logo.svg',
                 level: 50,
                 description: "Online platform for building and deploying web applications collaboratively."
             }, {
                 skillName: 'Canva',
-                imgSrc: 'canva-logo.svg',
+                imgFilename: 'canva-logo.svg',
                 level: 30,
                 description: "Graphic design tool offering templates for various visual content."
             },
             {
                 skillName: 'Vercel',
-                imgSrc: 'vercel-logo.svg',
+                imgFilename: 'vercel-logo.svg',
                 level: 30,
                 description: "Hosting platform for deploying and managing web applications."
             }, {
                 skillName: 'Netlify',
-                imgSrc: 'netlify-logo.svg',
+                imgFilename: 'netlify-logo.svg',
                 level: 30,
                 description: "Web hosting and serverless platform for modern development workflows."
             },
             {
                 skillName: 'NPM',
-                imgSrc: 'npm-logo.svg',
+                imgFilename: 'npm-logo.svg',
                 level: 60,
                 description: "Node Package Manager, used for installing and managing JavaScript packages and libraries."
             }
@@ -180,7 +180,7 @@ export default function Skills() {
                 <Skill
                     index={i}
                     skillName={techSkill.skillName}
-                    imgSrc={(skillKey == 'tools' ? toolPath : techPath) + techSkill.imgSrc}
+                    imgSrc={(skillKey == 'tools' ? toolPath : techPath) + techSkill.imgFilename}
                     overallStop={skillsData[skillKey].length * delAdd}
                     delay={i * delAdd}
                     level={techSkill.level}
